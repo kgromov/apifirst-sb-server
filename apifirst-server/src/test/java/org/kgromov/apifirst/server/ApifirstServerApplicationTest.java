f473752f5064110bb2f0905e10e558d2eac65416
konstantin <konst.gromov@gmail.com>
2024-04-09 00:03:49 +0300

    Setup multimodel project; add apifirst-server

---
+++ apifirst-server/src/test/java/org/kgromov/apifirst/server/ApifirstServerApplicationTest.java
@@ -0,0 +1,11 @@
+package org.kgromov.apifirst.server;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ApifirstServerApplicationTest {
+    @Test
+    void contextLoads() {
+    }
+
+}
