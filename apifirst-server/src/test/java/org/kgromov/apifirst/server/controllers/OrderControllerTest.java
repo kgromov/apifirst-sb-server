fb41f76d68d274b340d04b126d41c86c7a296e84
konstantin <konst.gromov@gmail.com>
2024-04-10 01:34:11 +0300

    Add order controller tests

--- apifirst-server/src/test/java/org/kgromov/apifirst/server/controllers/OrderControllerTest.java
+++ apifirst-server/src/test/java/org/kgromov/apifirst/server/controllers/OrderControllerTest.java
@@ -3,29 +3,29 @@ package org.kgromov.apifirst.server.controllers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.kgromov.apifirst.model.Product;
+import org.kgromov.apifirst.model.Order;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 
 import static org.hamcrest.Matchers.greaterThan;
-import static org.kgromov.apifirst.server.controllers.ProductController.BASE_URL;
+import static org.kgromov.apifirst.server.controllers.OrderController.BASE_URL;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
-class ProductControllerTest extends BaseE2ETest {
-    private Product testProduct;
+class OrderControllerTest extends BaseE2ETest {
+    private Order testOrder;
 
     @BeforeEach
     void setUp() {
         super.setUp();
-        this.testProduct = productRepository.findAll().iterator().next();
+        this.testOrder = orderRepository.findAll().iterator().next();
         // workaround from https://bitbucket.org/atlassian/swagger-request-validator/issues/406/path-params-dont-work-with-openapi-version
         System.setProperty("bind-type", "true");
     }
 
-    @DisplayName("Test get all orders")
+    @DisplayName("Test get all products")
     @Test
     void getCustomers() throws Exception {
         mockMvc.perform(get(BASE_URL)
@@ -34,12 +34,12 @@ class ProductControllerTest extends BaseE2ETest {
                 .andExpect(jsonPath("$.size()", greaterThan(0)));
     }
 
-    @DisplayName("Test get existed order by id")
+    @DisplayName("Test get existed product by id")
     @Test
     void getCustomerById() throws Exception {
-        mockMvc.perform(get(BASE_URL + "/{productId}", testProduct.getId())
+        mockMvc.perform(get(BASE_URL + "/{orderId}", testOrder.getId())
                         .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.id").value(testProduct.getId().toString()));
+                .andExpect(jsonPath("$.id").value(testOrder.getId().toString()));
     }
 }
